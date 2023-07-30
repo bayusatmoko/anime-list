e1ca9e11fbea968886929efb9004058ff5671711
bayusatmoko <bsatmoko@gmail.com>
2023-07-30 19:29:43 +0700

    delete unused env

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,9 +2,3 @@ declare module '*.css';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
-
-namespace NodeJS {
-    interface ProcessEnv {
-      REACT_APP_GRAPHQL_API_URL: string;
-    }
-  }
